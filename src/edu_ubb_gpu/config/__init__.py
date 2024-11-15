7f042f061f5d6d74c1757dbe2d254062abb03d3a
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2024-11-15 15:55:15 +0200

    removed duplicated files

--- src/edu_ubb_gpu/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from edu_ubb_gpu.config._settings import Settings
-
-
-__all__ = ["Settings"]
--- src/edu_ubb_gpu/mlflow/wrappers/__init__.py
+++
@@ -1,5 +0,0 @@
-from edu_ubb_gpu.mlflow.wrappers._sklearn import sklearn_model
-from edu_ubb_gpu.mlflow.wrappers._torch import torch_model
-
-
-__all__ = ["sklearn_model", "torch_model"]
